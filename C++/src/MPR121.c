ce1f6b5b01616ef73421bfff35527e9b0dc82469
Leonardo La Rocca <leo_la_rocca@yahoo.it>
2023-10-15 02:26:08 +0200

    feat(mpr121): working reset config function

--- C++/src/MPR121.c
+++ C++/src/MPR121.c
@@ -18,19 +18,66 @@ void MPR121_get_touch_status(uint16_t *val)
 
 void MPR121_reset()
 {
-    // Soft reset: write 0x63 to register 0x80
+    // Soft reset
     MPR121_write_reg(MPR121_SOFT_RESET_REGISTER, 0x63);
+
+    // Reset electrode configuration to defaults - enter stop mode
+    // Config registers are read-only unless in stop mode
+    MPR121_stop();
+
+
+    // Check CDT, SFI, ESI configuration is at defaults
+    // A soft reset puts CONFIG2 (0x5D) at 0x24
+    // Charge Discharge Time, CDT=1 (0.5us charge time)
+    // Second Filter Iterations, SFI=0 (4x samples taken)
+    // Electrode Sample Interval, ESI=4 (16ms period)
+    //if self._register8(MPR121_CONFIG2) != 0x24:
+    //    raise RuntimeError('Failed to reset MPR121 to default state')
+
+    // Set touch and release trip thresholds
+    MPR121_set_thresholds(MPR121_TOUCH_THRESHOLD_DEFAULT, MPR121_RELEASE_THRESHOLD_DEFAULT);
+
+    // Configure electrode filtered data and baseline registers
+    MPR121_write_reg(MPR121_MAX_HALF_DELTA_RISING_REGISTER, 0x01);
+    MPR121_write_reg(MPR121_MAX_HALF_DELTA_FALLING_REGISTER, 0x01);
+    MPR121_write_reg(MPR121_NOISE_HALF_DELTA_RISING_REGISTER, 0x01);
+    MPR121_write_reg(MPR121_NOISE_HALF_DELTA_FALLING_REGISTER, 0x05);
+    MPR121_write_reg(MPR121_NOISE_HALF_DELTA_TOUCHED_REGISTER, 0x00);
+    MPR121_write_reg(MPR121_NOISE_COUNT_LIMIT_RISING_REGISTER, 0x0E);
+    MPR121_write_reg(MPR121_NOISE_COUNT_LIMIT_FALLING_REGISTER, 0x01);
+    MPR121_write_reg(MPR121_NOISE_COUNT_LIMIT_TOUCHED_REGISTER, 0x00);
+    MPR121_write_reg(MPR121_FILTER_DELAY_COUNT_RISING_REGISTER, 0x00);
+    MPR121_write_reg(MPR121_FILTER_DELAY_COUNT_FALLING_REGISTER, 0x00);
+    MPR121_write_reg(MPR121_FILTER_DELAY_COUNT_TOUCHED_REGISTER, 0x00);
+
+    // Set config registers
+    // Debounce Touch, DT=0 (increase up to 7 to reduce noise)
+    // Debounce Release, DR=0 (increase up to 7 to reduce noise)
+    MPR121_write_reg(MPR121_DEBOUNCE_REGISTER, 0x00);
+    // First Filter Iterations, FFI=0 (6x samples taken)
+    // Charge Discharge Current, CDC=16 (16uA)
+    MPR121_write_reg(MPR121_CONFIG1_REGISTER, 0x10);
+    // Charge Discharge Time, CDT=1 (0.5us charge time)
+    // Second Filter Iterations, SFI=0 (4x samples taken)
+    // Electrode Sample Interval, ESI=0 (1ms period)
+    MPR121_write_reg(MPR121_CONFIG2_REGISTER, 0x20);
+
+    // Enable all electrodes - enter run mode
+    // Calibration Lock, CL=10 (baseline tracking enabled, initial value 5 high bits)
+    // Proximity Enable, ELEPROX_EN=0 (proximity detection disabled)
+    // Electrode Enable, ELE_EN=15 (enter run mode for 12 electrodes)
+    MPR121_run();
 }
 
 void MPR121_run()
 {
-    // Run mode: enable the first 8 electrodes
-    MPR121_write_reg(MPR121_ELECTRODE_CONFIG_REGISTER, 0x08);
+    // Run mode: enable all electrodes
+    MPR121_write_reg(MPR121_ELECTRODE_CONFIG_REGISTER, 0x8F);
 }
 
 void MPR121_stop()
 {
-    // Stop mode: write 0x00 to register 0x5E
+    // Stop mode: disable electrodes
     // TODO: keep first two bits of register
     MPR121_write_reg(MPR121_ELECTRODE_CONFIG_REGISTER, 0x00);
 }
--- C++/src/MPR121.h
+++ C++/src/MPR121.h
@@ -9,13 +9,29 @@
 // Registers
 #define MPR121_TOUCH_STATUS_0_REGISTER 0x00
 #define MPR121_TOUCH_STATUS_1_REGISTER 0x01
+// (0x2B~0x40) Baseline Filtering Control
+#define MPR121_MAX_HALF_DELTA_RISING_REGISTER 0x2B      // Max half delta (rising)
+#define MPR121_NOISE_HALF_DELTA_RISING_REGISTER 0x2C    // Noise half delta (rising)
+#define MPR121_NOISE_COUNT_LIMIT_RISING_REGISTER 0x2D   // Noise count limit (rising)
+#define MPR121_FILTER_DELAY_COUNT_RISING_REGISTER 0x2E  // Filter delay count (rising)
+#define MPR121_MAX_HALF_DELTA_FALLING_REGISTER 0x2F     // Max half delta (falling)
+#define MPR121_NOISE_HALF_DELTA_FALLING_REGISTER 0x30   // Noise half delta (falling)
+#define MPR121_NOISE_COUNT_LIMIT_FALLING_REGISTER 0x31  // Noise count limit (falling)
+#define MPR121_FILTER_DELAY_COUNT_FALLING_REGISTER 0x32 // Filter delay count (falling)
+// There is no max half delta for touched
+#define MPR121_NOISE_HALF_DELTA_TOUCHED_REGISTER 0x33   // Noise half delta (touched)
+#define MPR121_NOISE_COUNT_LIMIT_TOUCHED_REGISTER 0x34  // Noise count limit (touched)
+#define MPR121_FILTER_DELAY_COUNT_TOUCHED_REGISTER 0x35 // Filter delay count (touched)
 #define MPR121_TOUCH_THRESHOLD_0_REGISTER 0x41
 #define MPR121_RELEASE_THRESHOLD_0_REGISTER 0x42
+#define MPR121_DEBOUNCE_REGISTER 0x5B
+// (0x5C~0x5D) Filter and global CDC CDT configuration
+#define MPR121_CONFIG1_REGISTER 0x5C // FFI (first filter iterations), CDC (charge/discharge current)
+#define MPR121_CONFIG2_REGISTER 0x5D // CDT (charge/discharge time), SFI (second filter iterations), ESI (electrode sample interval)
 #define MPR121_ELECTRODE_CONFIG_REGISTER 0x5E
 #define MPR121_AUTO_CONFIG_0_REGISTER 0x7B
 #define MPR121_AUTO_CONFIG_1_REGISTER 0x7C
 #define MPR121_SOFT_RESET_REGISTER 0x80
-
 
 // Default values
 #define MPR121_TOUCH_THRESHOLD_DEFAULT 12
@@ -30,6 +46,5 @@ void MPR121_reset();
 void MPR121_run();
 void MPR121_set_thresholds(uint8_t touch, uint8_t release);
 void MPR121_stop();
-
 
 #endif // Melopero_MPR121_H_INCLUDED
