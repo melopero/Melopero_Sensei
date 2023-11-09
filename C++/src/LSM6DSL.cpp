cecbe3931bebabcc58131567291a6e8b767f7e23
Leonardo La Rocca <leo_la_rocca@yahoo.it>
2023-11-09 18:32:57 +0100

    fix: fixed imu i2c communication and example

--- C++/src/LSM6DSL.cpp
+++ C++/src/LSM6DSL.cpp
@@ -8,13 +8,13 @@ extern "C"
 
 int32_t platform_read(void *handle, uint8_t reg, uint8_t *buf, uint16_t len)
 {
-    I2C_read_reg(LSM6DSL_I2C_ADD_H, reg, buf, len, true);
+    I2C_read_reg(LSM6DSL_ID, reg, buf, len, true);
     return 0;
 }
 
 int32_t platform_write(void *handle, uint8_t reg, const uint8_t *buf, uint16_t len)
 {
-    I2C_write_reg(LSM6DSL_I2C_ADD_H, reg, buf, len, true);
+    I2C_write_reg(LSM6DSL_ID, reg, buf, len, true);
     return 0;
 }
 
@@ -36,26 +36,29 @@ void LSM6DSL::testSetup()
     } while (rst);
 
     /* Enable Block Data Update */
-    //lsm6dsl_block_data_update_set(&dev_ctx, PROPERTY_ENABLE);
+    lsm6dsl_block_data_update_set(&dev_ctx, PROPERTY_ENABLE);
     /* Set Output Data Rate */
-    //lsm6dsl_xl_data_rate_set(&dev_ctx, LSM6DSL_XL_ODR_12Hz5);
-    //lsm6dsl_gy_data_rate_set(&dev_ctx, LSM6DSL_GY_ODR_12Hz5);
+    lsm6dsl_xl_data_rate_set(&dev_ctx, LSM6DSL_XL_ODR_12Hz5);
+    lsm6dsl_gy_data_rate_set(&dev_ctx, LSM6DSL_GY_ODR_12Hz5);
     /* Set full scale */
-    //lsm6dsl_xl_full_scale_set(&dev_ctx, LSM6DSL_2g);
-    //lsm6dsl_gy_full_scale_set(&dev_ctx, LSM6DSL_2000dps);
+    lsm6dsl_xl_full_scale_set(&dev_ctx, LSM6DSL_2g);
+    lsm6dsl_gy_full_scale_set(&dev_ctx, LSM6DSL_2000dps);
     /* Configure filtering chain(No aux interface) */
     /* Accelerometer - analog filter */
-    //lsm6dsl_xl_filter_analog_set(&dev_ctx, LSM6DSL_XL_ANA_BW_400Hz);
+    lsm6dsl_xl_filter_analog_set(&dev_ctx, LSM6DSL_XL_ANA_BW_400Hz);
     /* Accelerometer - LPF1 path ( LPF2 not used )*/
-    // lsm6dsl_xl_lp1_bandwidth_set(&dev_ctx, LSM6DSL_XL_LP1_ODR_DIV_4);
+    lsm6dsl_xl_lp1_bandwidth_set(&dev_ctx, LSM6DSL_XL_LP1_ODR_DIV_4);
     /* Accelerometer - LPF1 + LPF2 path */
-    //lsm6dsl_xl_lp2_bandwidth_set(&dev_ctx,
-    //                             LSM6DSL_XL_LOW_NOISE_LP_ODR_DIV_100);
+    lsm6dsl_xl_lp2_bandwidth_set(&dev_ctx,
+                                 LSM6DSL_XL_LOW_NOISE_LP_ODR_DIV_100);
     /* Accelerometer - High Pass / Slope path */
-    // lsm6dsl_xl_reference_mode_set(&dev_ctx, PROPERTY_DISABLE);
-    // lsm6dsl_xl_hp_bandwidth_set(&dev_ctx, LSM6DSL_XL_HP_ODR_DIV_100);
+    lsm6dsl_xl_reference_mode_set(&dev_ctx, PROPERTY_DISABLE);
+    lsm6dsl_xl_hp_bandwidth_set(&dev_ctx, LSM6DSL_XL_HP_ODR_DIV_100);
     /* Gyroscope - filtering chain */
-    //lsm6dsl_gy_band_pass_set(&dev_ctx, LSM6DSL_HP_260mHz_LP1_STRONG);
+    lsm6dsl_gy_band_pass_set(&dev_ctx, LSM6DSL_HP_260mHz_LP1_STRONG);
+
+    /* Enable Block Data Update */
+    lsm6dsl_block_data_update_set(&dev_ctx, PROPERTY_ENABLE);
 }
 
 void LSM6DSL::updateMeasurements()
